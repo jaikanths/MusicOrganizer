5a9c61b327e2e99e323078fafe30dae99c8a1f65
Jaikanth Sithambaranathan <jaikanth.sk@hotmail.com>
2016-05-11 21:30:59 -0400

    Initial Version

---
+++ MusicOrganizer/FrmCopier.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace MusicOrganizer
+{
+    partial class FrmCopier
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblDirectory = new System.Windows.Forms.Label();
+            this.lblSong = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblExtensions = new System.Windows.Forms.Label();
+            this.lblDestination = new System.Windows.Forms.Label();
+            this.lblSource = new System.Windows.Forms.Label();
+            this.lblFileCount = new System.Windows.Forms.Label();
+            this.destination = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select Source";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblDirectory
+            // 
+            this.lblDirectory.AutoSize = true;
+            this.lblDirectory.Location = new System.Drawing.Point(122, 200);
+            this.lblDirectory.Name = "lblDirectory";
+            this.lblDirectory.Size = new System.Drawing.Size(0, 13);
+            this.lblDirectory.TabIndex = 1;
+            // 
+            // lblSong
+            // 
+            this.lblSong.AutoSize = true;
+            this.lblSong.Location = new System.Drawing.Point(122, 230);
+            this.lblSong.Name = "lblSong";
+            this.lblSong.Size = new System.Drawing.Size(0, 13);
+            this.lblSong.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Album";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 230);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Song";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Extensions";
+            // 
+            // lblExtensions
+            // 
+            this.lblExtensions.AutoSize = true;
+            this.lblExtensions.Location = new System.Drawing.Point(125, 265);
+            this.lblExtensions.Name = "lblExtensions";
+            this.lblExtensions.Size = new System.Drawing.Size(0, 13);
+            this.lblExtensions.TabIndex = 6;
+            // 
+            // lblDestination
+            // 
+            this.lblDestination.AutoSize = true;
+            this.lblDestination.Location = new System.Drawing.Point(190, 115);
+            this.lblDestination.Name = "lblDestination";
+            this.lblDestination.Size = new System.Drawing.Size(131, 13);
+            this.lblDestination.TabIndex = 7;
+            this.lblDestination.Text = "g:\\users\\jaikanth\\desktop";
+            // 
+            // lblSource
+            // 
+            this.lblSource.AutoSize = true;
+            this.lblSource.Location = new System.Drawing.Point(190, 72);
+            this.lblSource.Name = "lblSource";
+            this.lblSource.Size = new System.Drawing.Size(111, 13);
+            this.lblSource.TabIndex = 8;
+            this.lblSource.Text = "g:\\users\\public\\music";
+            // 
+            // lblFileCount
+            // 
+            this.lblFileCount.AutoSize = true;
+            this.lblFileCount.Location = new System.Drawing.Point(37, 297);
+            this.lblFileCount.Name = "lblFileCount";
+            this.lblFileCount.Size = new System.Drawing.Size(59, 13);
+            this.lblFileCount.TabIndex = 9;
+            this.lblFileCount.Text = "Reading....";
+            // 
+            // destination
+            // 
+            this.destination.Location = new System.Drawing.Point(37, 105);
+            this.destination.Name = "destination";
+            this.destination.Size = new System.Drawing.Size(115, 23);
+            this.destination.TabIndex = 10;
+            this.destination.Text = "Select Destination";
+            this.destination.UseVisualStyleBackColor = true;
+            this.destination.Click += new System.EventHandler(this.destination_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(114, 160);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(121, 23);
+            this.btnStart.TabIndex = 11;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 327);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Title";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(111, 327);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(0, 13);
+            this.lblTitle.TabIndex = 13;
+            // 
+            // FrmCopier
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1206, 555);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.destination);
+            this.Controls.Add(this.lblFileCount);
+            this.Controls.Add(this.lblSource);
+            this.Controls.Add(this.lblDestination);
+            this.Controls.Add(this.lblExtensions);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblSong);
+            this.Controls.Add(this.lblDirectory);
+            this.Controls.Add(this.button1);
+            this.Name = "FrmCopier";
+            this.Text = "Copier";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblDirectory;
+        private System.Windows.Forms.Label lblSong;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblExtensions;
+        private System.Windows.Forms.Label lblDestination;
+        private System.Windows.Forms.Label lblSource;
+        private System.Windows.Forms.Label lblFileCount;
+        private System.Windows.Forms.Button destination;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblTitle;
+    }
+}
+
---
+++ MusicOrganizer/FrmCopier.cs
@@ -0,0 +1,54 @@
+﻿using MusicFileService;
+using System;
+using System.Windows.Forms;
+
+namespace MusicOrganizer
+{
+    public partial class FrmCopier : Form
+    {
+        FolderBrowserDialog _dialog = new FolderBrowserDialog();
+        public FrmCopier()
+        {
+            InitializeComponent();
+            lblDirectory.Text = "Click start to iterate";
+            lblSong.Text = "No Song to Display";
+            lblExtensions.Text = "Extensions will be displayed here";
+            EnableStart();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _dialog.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            if (_dialog.ShowDialog(this) == DialogResult.OK)
+            {
+                lblSource.Text = _dialog.SelectedPath;
+            }
+            EnableStart();
+        }
+
+        private void destination_Click(object sender, EventArgs e)
+        {
+            _dialog.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            if (_dialog.ShowDialog(this) == DialogResult.OK)
+            {
+                lblDestination.Text = _dialog.SelectedPath;
+            }
+            EnableStart();
+        }
+
+        private void EnableStart()
+        {
+            btnStart.Enabled = !string.IsNullOrWhiteSpace(lblSource.Text) && !string.IsNullOrWhiteSpace(lblDestination.Text);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            var copier = new MusicFileCopier(lblSource.Text, lblDestination.Text);
+            var statusDisplay = new CopyStatusDisplay { Album = lblDirectory, Count = lblFileCount, Extensions = lblExtensions, Song = lblSong, Title = lblTitle, ShowStatus = ((Master)MdiParent).ShowStatus };
+            var statusUpdater = new MusicFileCopyStatusUpdater(statusDisplay);
+            copier.Subscribe(statusUpdater);
+            copier.Copy();
+        }
+    }
+}
